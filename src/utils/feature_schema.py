e0fcf760184d289ad8820555d9d5ef5de653ba27
koushik <bkoushik81@gmail.com>
2026-01-30 11:29:19 +0530

    Feature Engineering and Selecting final features for model training

---
+++ src/utils/feature_schema.py
@@ -0,0 +1,19 @@
+# src/utils/feature_schema.py
+
+BASE_PROVIDER_COLS = [
+    'Provider',
+    'PotentialFraud'
+]
+
+RAW_PROVIDER_AGG_COLS = [
+    'PRV_Insc_Clm_ReImb_Amt',
+    'PRV_IP_Annual_ReImb_Amt',
+    'PRV_OP_Annual_ReImb_Amt',
+    'PRV_IP_Annual_Ded_Amt',
+    'PRV_OP_Annual_Ded_Amt',
+    'PRV_CoPayment',
+    'PRV_Admit_Duration',
+    'PRV_Claim_Duration',
+    'PRV_Bene_Age_Sum',
+    'PRV_Tot_RKD_Patients'
+]
